492e300892f1059e2e2f89561515058f82f87d22
Maanisha G <maanisha.govindasamy@gmail.com>
2026-08-11 15:47:05 +0530

    Add files via upload

---
+++ age clean.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+data={
+    "Name":["Arun","Priya","Ravi","Arun","Divya"],
+    "Age":[21,np.nan ,20,21,22],
+    "City":["Salem","Chennai","Salem","SALEM","Unknown"],
+    "Salary":[30000,45000,np.nan,30000,50000]
+}
+pd=pd.DataFrame(data)
+pd["Age"]=pd["Age"].fillna(pd["Age"].mean())
+print(pd)
