b703609c00b8a2f836c485e06bffe4385322660f
Maanisha G <maanisha.govindasamy@gmail.com>
2026-08-11 15:43:57 +0530

    Add files via upload

---
+++ isnull.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+data={
+    "Name":["Arun","Priya","Ravi","Arun","Divya"],
+    "Age":[21,np.nan ,20,21,22],
+    "City":["Salem","Chennai","Salem","SALEM","Unknown"],
+    "Salary":[30000,45000,np.nan,30000,50000]
+}
+pd=pd.DataFrame(data)
+print(pd.isnull())
